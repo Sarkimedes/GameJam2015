a25def6b3082b3d026a0da307dfb76adb73a7b90
Peter Roberts <peter@blackmarble.com>
2015-06-11 14:29:25 -0700

    Added unit test project

---
+++ GameJam2015MonoGame.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameJam2015MonoGame.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
